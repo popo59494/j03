4478f99a5a23163b926b21934235431cc595605f
macbook <macbook@MacBook-Pro-de-macbook-2.local>
2018-07-10 01:39:07 +0200

    ex08

---
+++ ex08/ft_atoi.c
@@ -0,0 +1,26 @@
+int	ft_atoi(char *str)
+{
+	int a;
+	int b;
+	int c;
+
+	a = 0;
+	b = 0;
+	while(str[i] != '\0')
+		i = i + 1;
+	c = i
+	if (str[0] == '-')
+		
+	while (b < c)
+		{
+		while (i>0)
+			{
+				a = a + ((str[b] - '0'))*(10^(i-1)
+				b = b + 1;
+				i = i - 1;
+			}
+		}
+	return(a)
+}
+
+100+20+
